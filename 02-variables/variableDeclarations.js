86cf9a4937a51468f1ae68a4799b89f8fe1e5164
Rodrigo Bonzerr S. Lopez <rodprogramdev@gmail.com>
2023-04-04 10:42:02 +0800

    Add declareVar.html

--- 02-variables/variableDeclarations.js
+++ 02-variables/variableDeclarations.js
@@ -1,20 +1,46 @@
 
 /* WAYS OF DECLARING VARIABLES */
 
+// The var keyword is used for writing JS code since 1995 to 2015
 var a; // If we use the var keyword we are expecting some ramifications in relation to scope
-let b; // Let keyword
-const PLAYER = 'c'; // const keyword
-y = 5; // undeclared variables
 
+// The let and const keywords were added to JS in 2015
+let b; // Let keyword
+const PLAYER1 = 'character'; // const keyword. We can use single quotes to assign a string data type
+const PLAYER2 = "character"; // const keyword. WE can also use double quotes to assign a string data type
+
+
+y = 5; // undeclared variables
 
 var x; // Declaring a variable
 
 /*INITIALIZING OR ASSIGNING A VALUE TO A VARIABLE > We are storing data inside of a variable */
 var x = 10; // Initializing a variable or assigning a value to a variable. 
 
+/* We can perform mathematical operations using variables*/
+var sum = y + x;
 
-console.log(typeof a);
-console.log(b);
-console.log(typeof PLAYER);
-console.log(typeof y);
-console.log(typeof x);
+/* WE can check for yes or no or true or false using variables*/
+
+let d = false; // This is a boolean data type.
+
+
+/*Data Type of string, number, boolean, and undefined  */
+
+/*GENERAL RULE */
+
+// 1. We want to always declare variables with const > constant variables , when we don't want to change the value inside of the variable.
+// 2. If we think we will need to change the value of a variable we can use the let keyword. 
+
+
+
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof PLAYER1);
+// console.log(typeof PLAYER2);
+// console.log(typeof y);
+// console.log(typeof x);
+// console.log(sum);
+// console.log(typeof d);
+
+
