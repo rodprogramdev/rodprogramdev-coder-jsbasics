00176fa5a038cb6bce1ee30529a88c322fd90d01
Rodrigo Bonzerr S. Lopez <rodprogramdev@gmail.com>
2023-04-04 10:25:01 +0800

    Examples of declaring, initializing and checking data type of  a variable

---
+++ 02-variables/variableDeclarations.js
@@ -0,0 +1,20 @@
+
+/* WAYS OF DECLARING VARIABLES */
+
+var a; // If we use the var keyword we are expecting some ramifications in relation to scope
+let b; // Let keyword
+const PLAYER = 'c'; // const keyword
+y = 5; // undeclared variables
+
+
+var x; // Declaring a variable
+
+/*INITIALIZING OR ASSIGNING A VALUE TO A VARIABLE > We are storing data inside of a variable */
+var x = 10; // Initializing a variable or assigning a value to a variable. 
+
+
+console.log(typeof a);
+console.log(b);
+console.log(typeof PLAYER);
+console.log(typeof y);
+console.log(typeof x);
