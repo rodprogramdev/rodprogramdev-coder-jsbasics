f863cc516b0b0ef96e01b5353ee95f5d9becee00
Rodrigo Bonzerr S. Lopez <rodprogramdev@gmail.com>
2023-04-11 10:33:52 +0800

    Examples of expression in javascript

---
+++ 04-JavascriptSyntaxExpressions&operators/expressions.js
@@ -0,0 +1,71 @@
+/*JS Syntax will try to define to types of values
+1. Fixed values are called literals. // Numbers and Strings 'Rod' , "Rod"
+2. Variable values are called variables. 
+*/
+
+/* We can compare with varius types of speech
+
+Statements in JavaScript will be executed sequentially.
+
+A Statement is composed of one or more expressions. 
+
+An expression is made up of operators and operands.
+
+
+
+*/
+
+/*OPERATORS*/
+
+/*
+Arithmetic Operators + - * / 
+
++  Plus sign > "String concatenation operator" , "Adding values"
+
+= Equal sign , "Assignment operator" 
+
+
+
+
+*/
+
+
+/*OPERANDS*/
+
+// We as the programmer can give operands their name.
+
+
+/*EXPRESSIONS - these are a combination of values, variables and opertors*/
+
+
+// Variable declaration
+let a;
+
+
+// Variable initialization or assigning a value
+
+a = 4;
+
+
+// Expressions > Evaluating a value 10 + 20 = 30
+
+let b = 10;
+let c = 20;
+
+
+let e = b + c; // We have three expressions in this single line statement.
+
+// First: let e; ... variable declaration
+// Second: perform an evaluation ... b + c > Is adding to values 
+// Third: Assigning a value assign the result of b + c to variable e
+
+
+
+// a;
+// console.log(a); // a is not defined. 
+
+
+console.log(a); // a is not defined. 
+
+
+
