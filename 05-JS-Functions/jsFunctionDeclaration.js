369ea0fa5fd9e1e6167715d3e75bae5d89903e67
Rodrigo Bonzerr S. Lopez <rodprogramdev@gmail.com>
2023-04-19 10:52:48 +0800

    Add working introduce() function

--- 05-JS-Functions/jsFunctionDeclaration.js
+++ 05-JS-Functions/jsFunctionDeclaration.js
@@ -5,12 +5,12 @@
 // It is a block of code that we the programmers can name and then call. 
 
 /*
-function test(num1,num2){
+function test(num1,num2){ // Parameters are lsted inside the parentheses for a function definition 
     // instructions go here. 
     let product =  num1 * num2;
     console.log(product);
 }
-test(5,5);
+test(5,5); // function Arguments which are values that is received by the function when we invoke or call the function. 
 */
 
 
@@ -23,9 +23,29 @@ test(5,5);
 4. It can contain dollar signs
 */
 
-function declareName(){ // declare a function name
+/*
+function declareName(parameter1, parameter2){ // declare a function name
     let name; // declare a variable
     console.log('------------------------');
+    
+} 
+*/
+
+
+function introduce(){ // declare a function name
+    let name; // declare a variable
+    console.log('------------------------');
+    name = prompt("Enter your First and Last name here:");
+
+    console.log('Hello ' + name);
+    
 } 
 
-declareName();
+// FUNCTION INVOCATION 
+/*
+1. When an even occurs such as a user clicks a button.
+2. When it is invoked or called from a javascript code. 
+3. IIFE > Doing self invoked or automatically invoking itself. 
+*/
+
+introduce();
