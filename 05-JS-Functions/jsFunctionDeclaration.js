433ee5e2b2aabb7470e88cf65ce8de4167c33bc0
Rodrigo Bonzerr S. Lopez <rodprogramdev@gmail.com>
2023-04-19 10:37:01 +0800

    add second example of Fundction declaration

---
+++ 05-JS-Functions/jsFunctionDeclaration.js
@@ -0,0 +1,31 @@
+// It is a block of code that is designed to perform a particular task. 
+// A function can be exectued when "something" invokes it. (Calls it);
+
+
+// It is a block of code that we the programmers can name and then call. 
+
+/*
+function test(num1,num2){
+    // instructions go here. 
+    let product =  num1 * num2;
+    console.log(product);
+}
+test(5,5);
+*/
+
+
+
+
+/*FUNCTION NAMES
+1. It can contain letters
+2. It can contain digits
+3. It can contain underscores
+4. It can contain dollar signs
+*/
+
+function declareName(){ // declare a function name
+    let name; // declare a variable
+    console.log('------------------------');
+} 
+
+declareName();
