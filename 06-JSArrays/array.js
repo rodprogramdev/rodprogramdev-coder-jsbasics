70db0791e469ef76850250a33762f9832efced55
Rodrigo Bonzerr S. Lopez <rodprogramdev@gmail.com>
2023-06-04 17:41:53 +0800

    Add example1 of an array

---
+++ 06-JSArrays/array.js
@@ -0,0 +1,33 @@
+// var a = 1;
+// let b = 'Test Store on Arrays';
+// const c = true;
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+// let a = [1, 'Test array case for storing multiple values', true ];
+
+// console.log(a);
+
+let person1 = 'Man';
+let person2 = 'Woman';
+let person3 = 'Kid';
+
+console.log(person1);
+console.log(person2);
+console.log(person3);
+
+let person = ['Man', 'Woman', 'Kid'];
+
+console.log(person);
+
+
+
+
+
+
+
+
