5ac8dadddcbc59348db2d49465634738c6b2799d
Rodrigo Bonzerr S. Lopez <rodprogramdev@gmail.com>
2023-06-15 16:34:44 +0800

    Add if else if else statement examples

--- 07-ConditionalStatements/conditionalStatement.js
+++ 07-ConditionalStatements/conditionalStatement.js
@@ -4,18 +4,43 @@
 // switch statements, specify alternative blocks of code that we want to be executed. 
 
 
-var count = 5;
+// var count = 5;
 
 // if(/*We want to evaluate an expression*/){
 
 // }
 
 /*
-
 if(count == 4){
     console.log("count is 4");
-}*/
+}
+*/
+
+/**************************************************** */
+var count = 5;
 
 if(count == 5){
-    console.log("This condition is true and will display this text on the console.");
+    console.log("This 'if' statement is true and will display this text on the console.");
+} else{
+    console.log("This 'if' statement is false so this text should be executed on the console. ");
 }
+
+
+/**************************************************** */
+
+var counter = 4;
+
+if(counter == 2){
+    console.log("Counter is equal to 2.");
+}else if(counter > 2){
+    console.log("Counter is greater than 2.");
+}else if (counter < 2){
+    console.log("Counter is less than 2");
+}else{
+    console.log("Counter is NOT 2"); // this will not be executed if the other lines of instructions are true.
+}
+
+
+
+
+
