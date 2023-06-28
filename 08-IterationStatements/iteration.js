381b9b8c98251ff767623d4af8405f85b7073b04
Rodrigo Bonzerr S. Lopez <rodprogramdev@gmail.com>
2023-06-28 23:25:24 +0800

    Add itreation.js and index.html

---
+++ 08-IterationStatements/iteration.js
@@ -0,0 +1,30 @@
+// How we can loop through a body of code. 
+
+// JAVASCRIPT LOOPS
+
+// Loops > WE want a block of code to run a number of times until a condition is met.
+
+// We have a number of different iteration statements. Loop For, Loof For In, Loop For Of, Loop While
+
+// Relationship of loops to arrays. 
+
+// For Loop 
+
+/*
+> The for loop will have 3 optional expressions
+
+Expression 1 > Declare a variable i and assign it with a value of 0.
+Expression 2  > Define the condition for the loop to run > If i is less than 10 then execute the code block instruction console.log(i).
+Expression 3 > Increment or increase the value each time the code block in the loop is true or the condition in the loop has been met. 
+*/
+
+
+// for(let i=0; i<10; i++){
+//     console.log(i);
+// }
+
+
+// Shorthand version for a for loop for the first expression.
+for(i=0; i<10; i++){
+    console.log(i);
+}
